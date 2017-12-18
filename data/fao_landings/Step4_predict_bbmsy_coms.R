0a4787d6cc4ee2053cf55238cb3b58db62f9ea94
cfree14 <cfree14@gmail.com>
2017-12-18 15:09:33 -0500

    Added try() to execution of COMs

--- data/fao_landings/Step4_predict_bbmsy_coms.R
+++ data/fao_landings/Step4_predict_bbmsy_coms.R
@@ -2,9 +2,6 @@
 # Predict status of FAO stocks using catch-only models
 # By Chris Free, Rutgers University
 
-# SSCOM needs catch in time series
-# All methods probably need a try() statement and actions when method fails
-
 # Clear workspace
 rm(list = ls())
 
@@ -34,7 +31,7 @@ load(paste(datadir, "1950_2017_FAO_landings_data_use.Rdata", sep="/"))
 # Fit example models?
 fit_examples <- F
 if(fit_examples==F){
-  load(paste(datadir, "example_com_outputs.Rdata", sep="/"))
+  load(paste(preddir, "example_com_outputs.Rdata", sep="/"))
 }else{
   # Example stock
   i <- 52
@@ -72,7 +69,7 @@ if(fit_examples==F){
   
   # Save example model runs
   save(sscom_output, comsir_output, cmsy13_output, mprm_output,
-       file=paste(datadir, "example_com_outputs.Rdata", sep="/"))
+       file=paste(preddir, "example_com_outputs.Rdata", sep="/"))
   
 }
 
@@ -85,11 +82,11 @@ if(fit_examples==F){
 # bbmsy_q2.5, bbmsy_q25, bbmsy_q50, bbmsy_q75, bbmsy_q97.5 (bbmsy_avg, bbmsy_sd, convergence)
 
 # Which model?
-com_to_fit <- "cmsy13"
+com_to_fit <- "mprm"
 
 # Loop through stocks and fit model: i <- 1
- for(i in 1:3){
-# for(i in 1:nrow(stocks)){
+# for(i in 1:3){
+for(i in 1:nrow(stocks)){
   
   # Subset data
   stock <- stocks$stockid[i]
@@ -100,49 +97,60 @@ com_to_fit <- "cmsy13"
   
   # Fit mPRM
   if(com_to_fit=="mprm"){
-    mprm_data <- format_prm(year=sdata$year, catch=sdata$tl, bbmsy=NA, species_cat=lh_catg)
-    mprm_output <- predict_prm(mprm_data, model=datalimited::ram_prm_model, ci=T)
-    bbmsy_ts <- mprm_output %>% 
-      mutate(stockid=stock, method="mPRM") %>% 
-      select(stockid, method, everything())
+    bbmsy_ts <- try({
+      mprm_data <- format_prm(year=sdata$year, catch=sdata$tl, bbmsy=NA, species_cat=lh_catg)
+      mprm_output <- predict_prm(mprm_data, model=datalimited::ram_prm_model, ci=T)
+      bbmsy_ts <- mprm_output %>% 
+        mutate(stockid=stock, method="mPRM") %>% 
+        select(stockid, method, everything())
+    })
   }
   
   # Fit cMSY-13
   if(com_to_fit=="cmsy13"){
-    cmsy13_nreps <- 500000
-    cmsy13_output <- cmsy(yr=sdata$year, ct=sdata$tl, start_r=resilience(res), reps=cmsy13_nreps)
-    bbmsy_ts <- cmsy13_output$bbmsy %>% 
-      mutate(stockid=stock, method="cMSY-13") %>% 
-      rename(bbmsy_avg=bbmsy_mean) %>% 
-      select(stockid, method, year, catch, bbmsy_q2.5:bbmsy_q97.5, bbmsy_avg, bbmsy_sd)
+    bbmsy_ts <- try({
+      cmsy13_nreps <- 500000
+      cmsy13_output <- cmsy(yr=sdata$year, ct=sdata$tl, start_r=resilience(res), reps=cmsy13_nreps)
+      bbmsy_ts <- cmsy13_output$bbmsy %>% 
+        mutate(stockid=stock, method="cMSY-13") %>% 
+        rename(bbmsy_avg=bbmsy_mean) %>% 
+        select(stockid, method, year, catch, bbmsy_q2.5:bbmsy_q97.5, bbmsy_avg, bbmsy_sd)
+    })
   }
   
-  # Fit SSCOM
+  # Fit SSCOM (really slow)
   if(com_to_fit=="sscom"){
-    sscom_output <- sscom(yr=sdata$year, ct=sdata$tl, start_r=resilience(res))
-    bbmsy_ts <- sscom_output %>%
-      mutate(stockid=stock, method="SSCOM") %>% 
-      rename(bbmsy_q50=b_bmsy, bbmsy_q97.5=b_bmsyUpper, bbmsy_q2.5=b_bmsyLower, 
-             bbmsy_q25=b_bmsy_iq25, bbmsy_q75=b_bmsy_iq75) %>% 
-      select(stockid, method, year, bbmsy_q2.5, bbmsy_q25, bbmsy_q50, bbmsy_q75, bbmsy_q97.5, convergence)
+    bbmsy_ts <- try({
+      sscom_output <- sscom(yr=sdata$year, ct=sdata$tl, start_r=resilience(res))
+      bbmsy_ts <- sscom_output %>%
+        mutate(stockid=stock, method="SSCOM") %>% 
+        rename(bbmsy_q50=b_bmsy, bbmsy_q97.5=b_bmsyUpper, bbmsy_q2.5=b_bmsyLower, 
+               bbmsy_q25=b_bmsy_iq25, bbmsy_q75=b_bmsy_iq75) %>% 
+        select(stockid, method, year, bbmsy_q2.5, bbmsy_q25, bbmsy_q50, bbmsy_q75, bbmsy_q97.5, convergence)
+    })
   }
   
   # Fit COMSIR
   if(com_to_fit=="comsir"){
-    comsir_nposterior <- 1000000
-    comsir_nburnin <- nposterior*0.05
-    comsir_output <- comsir(yr=sdata$year, ct=sdata$tl, start_r=resilience(res),
-                            nsim=comsir_nburnin, n_posterior=comsir_nposterior)
-    bbmsy_ts <- comsir_output$bbmsy %>%
-      mutate(stockid=stock, method="COMSIR") %>% 
-      rename(bbmsy_avg=bbmsy_mean) %>%
-      select(stockid, method, year, catch, bbmsy_q2.5:bbmsy_q97.5, bbmsy_avg, bbmsy_sd)
+    bbmsy_ts <- try({
+      comsir_nposterior <- 1000000
+      comsir_nburnin <- nposterior*0.05
+      comsir_output <- comsir(yr=sdata$year, ct=sdata$tl, start_r=resilience(res),
+                              nsim=comsir_nburnin, n_posterior=comsir_nposterior)
+      bbmsy_ts <- comsir_output$bbmsy %>%
+        mutate(stockid=stock, method="COMSIR") %>% 
+        rename(bbmsy_avg=bbmsy_mean) %>%
+        select(stockid, method, year, catch, bbmsy_q2.5:bbmsy_q97.5, bbmsy_avg, bbmsy_sd)
+    })
   }
   
   # Save B/BMSY time series
-  if(i==1){bbmsy_ts_all <- bbmsy_ts}else{bbmsy_ts_all <- rbind(bbmsy_ts_all, bbmsy_ts)}
+  if(!inherits(bbmsy_ts, "try-error")){
+    if(i==1){bbmsy_ts_all <- bbmsy_ts}else{bbmsy_ts_all <- rbind(bbmsy_ts_all, bbmsy_ts)}
+  }
   
 }
+
 
 # Export model predictions
 ################################################################################
@@ -150,7 +158,6 @@ com_to_fit <- "cmsy13"
 # Export predictions
 outfile <- paste0("1950_2017_FAO_bbmsy_timeseries_", com_to_fit, ".csv")
 write.csv(bbmsy_ts_all, paste(preddir, outfile, sep="/"), row.names=F)
-
 
 
 
