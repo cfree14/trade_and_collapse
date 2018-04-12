8681482c9fd4a38d091055de00687a8cbcf8bd77
Chris Free <cfree14@gmail.com>
2018-04-12 14:25:42 -0400

    forced to commit

--- data/fao_landings/Step4_predict_bbmsy_coms.R
+++ data/fao_landings/Step4_predict_bbmsy_coms.R
@@ -90,7 +90,7 @@ if(fit_examples==F){
 # bbmsy_q2.5, bbmsy_q25, bbmsy_q50, bbmsy_q75, bbmsy_q97.5 (bbmsy_avg, bbmsy_sd, convergence)
 
 # Which model?
-com_to_fit <- "zbrt"
+com_to_fit <- "sscom"
 
 # Loop through stocks and fit model: i <- 1
 # for(i in 1:3){
@@ -141,8 +141,8 @@ for(i in 1:nrow(stocks)){
   # Fit COMSIR
   if(com_to_fit=="comsir"){
     bbmsy_ts <- try({
-      comsir_nposterior <- 1000000
-      comsir_nburnin <- nposterior*0.05
+      comsir_nposterior <- 2000000
+      comsir_nburnin <- comsir_nposterior*0.05
       comsir_output <- comsir(yr=sdata$year, ct=sdata$tl, start_r=resilience(res),
                               nsim=comsir_nburnin, n_posterior=comsir_nposterior)
       bbmsy_ts <- comsir_output$bbmsy %>%
@@ -164,7 +164,9 @@ for(i in 1:nrow(stocks)){
   
   # Save B/BMSY time series
   if(!inherits(bbmsy_ts, "try-error")){
-    if(i==1){bbmsy_ts_all <- bbmsy_ts}else{bbmsy_ts_all <- rbind(bbmsy_ts_all, bbmsy_ts)}
+    # if(i==1){bbmsy_ts_all <- bbmsy_ts}else{bbmsy_ts_all <- rbind(bbmsy_ts_all, bbmsy_ts)}
+    if(exists("bbmsy_ts_all")==F){bbmsy_ts_all <- bbmsy_ts}else{bbmsy_ts_all <- rbind(bbmsy_ts_all, bbmsy_ts)}
+    
   }
   
 }
