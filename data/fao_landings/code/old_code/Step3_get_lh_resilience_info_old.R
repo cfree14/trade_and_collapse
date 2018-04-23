11c9a1bc1dae98ac0d7a704e8f2e827ddd613d81
cfree14 <cfree14@gmail.com>
2018-04-23 15:23:54 -0400

    fixed up resilience and m assesmbly

--- data/fao_landings/code/old_code/Step3_get_lh_resilience_info_old.R
+++ data/fao_landings/code/old_code/Step3_get_lh_resilience_info_old.R
@@ -273,7 +273,9 @@ if(lh.cached){
   
   # Format life history data
   lhdata1 <- lhdata %>% 
-    mutate(temp=log(temp)) %>% 
+    mutate(temp=log(temp),
+           resilience=cut(k, breaks=c(0, 0.1, 0.2, 0.4, 8), 
+                          labels=c("Very low", "Low", "Medium", "High"))) %>% 
     rename(linf_cm=linf, winf_g=winf, tmax_yr=tmax, tmat_yr=tm, lmat_cm=lm, temp_c=temp)
 
   # Completeness
@@ -285,8 +287,13 @@ if(lh.cached){
 }
   
 
-# Merge FishBase info
+# Merge info
 ################################################################################
+
+
+
+# Format life history info
+##########################################
 
 # Mode of string
 # x <- c(NA, NA, NA, NA, rep("a", 3), rep("b", 2), rep("c", 1)); mode.string(x)
@@ -295,7 +302,7 @@ mode.string <- function(x) {
   ux[which.max(tabulate(match(x, ux)))]
 }
 
-# Merge invert/finish resilience data
+# Format resilience data
 # Reduce to 1 observation per species
 resilience <- stocks_fin %>%
   rbind.fill(stocks_inv) %>% 
@@ -304,8 +311,11 @@ resilience <- stocks_fin %>%
             resil_all=paste(na.omit(unique(Resilience)), collapse=", "),
             resil_mode=mode.string(Resilience)) %>% 
   filter(nresil>0)
+table(resilience$resil_mode)
 
-# Merge invert/finish growth data
+species_inv$Vulnerability[!is.na(species_inv$Vulnerability)]
+
+# Format Von B K data
 # Reduce to 1 observation per species
 vonb <- popgrowth_fin %>% 
   rbind.fill(popgrowth_inv) %>% 
@@ -314,30 +324,58 @@ vonb <- popgrowth_fin %>%
             linf_cm=median(Loo),
             m=median(M))
 
-# Merge invert/finish species information
+# Format basic data
 basic <- species_fin %>% 
   rbind.fill(species_inv)
 
-# Merge FishBase Info
-lhdata <- spp_lh_key %>% 
-  # Add FishBase base info
+
+# Resilience by genus and family keys
+##########################################
+
+# It turns out that assigning the genus mode doesn't fill that many gaps
+# Resiliences are either known/unknown across genera
+
+# Resilience by genus key
+resil_gen <- resilience %>% 
+  left_join(select(taxa_key, sciname, family, genus), by="sciname") %>% 
+  group_by(family, genus) %>% 
+  summarize(resilience=mode.string(resil_mode)) %>% 
+  ungroup()
+
+# Resilience by family key
+resil_fam <- resilience %>% 
+  left_join(select(taxa_key, sciname, family), by="sciname") %>% 
+  group_by(family) %>% 
+  summarize(resilience=mode.string(resil_mode)) %>% 
+  ungroup()
+
+# Resilience by order key
+resil_ord <- resilience %>% 
+  left_join(select(taxa_key, sciname, order), by="sciname") %>% 
+  group_by(order) %>% 
+  summarize(resilience=mode.string(resil_mode)) %>% 
+  ungroup()
+
+
+# Merge life history info
+##########################################
+
+# Merge life history data
+spp_lh_data <- spp_lh_key %>% 
+  # Add/format habitat/length/vulnerability info
   left_join(select(basic, sciname, DemersPelag, Vulnerability, Length, LongevityWild), by=c("sci_name_fb"="sciname")) %>% 
-  rename(habitat=DemersPelag, vulnerability_fb=Vulnerability, lmax_cm_fb=Length, tmax_yr_fb=LongevityWild) %>%
-  # Add FishBase resilience info
+  rename(habitat=DemersPelag, vulnerability=Vulnerability, lmax_cm=Length, tmax_yr=LongevityWild) %>%
+  # Add/format resilience info
   left_join(select(resilience, sciname, resil_mode), by=c("sci_name_fb"="sciname")) %>% 
   rename(resilience_fb=resil_mode) %>%
-  # Add FishBase life history info
+  left_join(select(resil_gen, genus, resilience), by="genus") %>% 
+  rename(resilience_gen=resilience) %>% 
+  left_join(resil_fam, by="family") %>% 
+  rename(resilience_fam=resilience) %>%
+  left_join(resil_ord, by="order") %>% 
+  rename(resilience_ord=resilience) %>%
+  # Add/format Von B K and natural mortality info
   left_join(vonb, by=c("sci_name_fb"="sciname")) %>% 
-  rename(vonb_k_fb=vonb_k, linf_cm_fb=linf_cm, m_fb=m) %>% 
-  # Add additional resilience categories
-  mutate(resilience_k_fb=as.character(cut(vonb_k_fb, breaks=c(0, 0.05, 0.15, 0.30, 8), labels=c("Very low", "Low", "Medium", "High"))),
-         resilience_vuln_fb=as.character(cut(vulnerability_fb, breaks=c(0, 30, 55, 65, 100), labels=c("High", "Medium", "Low", "Very low"))),
-         resilience_tmax_fb=as.character(cut(tmax_yr_fb, breaks=c(0, 3.5, 10.5, 30, 400), labels=c("High", "Medium", "Low", "Very low")))) %>% 
-  # Add FishLife life history info
-  left_join(lhdata1, by=c("sci_name_fb"="species")) %>% 
-  mutate(resilience_fl=as.character(cut(k, breaks=c(0, 0.05, 0.15, 0.30, 8), labels=c("Very low", "Low", "Medium", "High")))) %>% 
-  rename(tmax_yr_fl=tmax_yr, vonb_k_fl=k, linf_cm_fl=linf_cm, m_fl=m, 
-         winf_g_fl=winf_g, temp_c_fl=temp_c, lmat_cm_fl=lmat_cm) %>% 
   # Add life history categories consistent with mPRM
   left_join(select(lh_catg_key, sci_name_fb, lh_catg), by="sci_name_fb") %>% 
   mutate(lh_catg1=revalue(habitat, c("bathydemersal"="demersal",
@@ -353,56 +391,96 @@ lhdata <- spp_lh_key %>%
          lh_catg1=ifelse(sci_name_fb%in%c("Libinia emarginata", "Corallium sp. nov.", "Pharus legumen"), "demersal", lh_catg1),
          lh_catg2=lh_catg1,
          lh_catg2=ifelse(lh_catg1=="pelagic",
-                         ifelse((linf_cm_fl>=40 & !is.na(linf_cm_fl)) | (lmax_cm_fb>=50 & !is.na(lmax_cm_fb)), "large pelagic", "small pelagic"), lh_catg2)) %>% 
-  # Add final resilience
-  mutate(resilience=ifelse(!is.na(resilience_fb), resilience_fb, resilience_fl),
-         resilience=ifelse(is.na(resilience), resilience_k_fb, resilience),
-         resilience=ifelse(is.na(resilience), resilience_vuln_fb, resilience),
-         resilience=ifelse(is.na(resilience), resilience_tmax_fb, resilience)) %>% 
-  # Add resilience by genera
-  group_by(genus) %>% 
-    mutate(resilience_genus=mode.string(resilience)) %>% 
-  ungroup() %>% 
-  # Add resilience by family
-  group_by(family) %>% 
-  mutate(resilience_family=mode.string(resilience)) %>% 
-  ungroup() %>% 
-  # Add higher-level taxa resiliences
-  mutate(resilience=ifelse(is.na(resilience), resilience_genus, resilience),
-         resilience=ifelse(is.na(resilience), resilience_family, resilience)) %>% 
-  # Add final M
-  mutate(m_tmax_fb=4.899*tmax_yr_fb^-0.916,
-         m_vonb_fb=4.118*vonb_k_fb^0.73*linf_cm_fb^-0.33,
-         m=ifelse(!is.na(m_fl), m_fl, m_fb),
-         m=ifelse(is.na(m), m_tmax_fb, m),
-         m=ifelse(is.na(m), m_vonb_fb, m)) %>% 
-  # Add M by genera
-  group_by(genus) %>% 
-  mutate(m_genus=median(m, na.rm=T)) %>% 
-  ungroup() %>% 
-  # Add resilience by family
-  group_by(family) %>% 
-  mutate(m_family=median(m, na.rm=T)) %>% 
-  ungroup() %>% 
-  # Add higher-level taxa resiliences
-  mutate(m=ifelse(is.na(m), m_genus, m),
-         m=ifelse(is.na(m), m_family, m)) %>% 
-  # Rearrange columns
-  select(sci_name_orig:genus, habitat, 
-         lh_catg, lh_catg1, lh_catg2, 
-         resilience, resilience_fl, resilience_fb, 
-         resilience_k_fb, resilience_vuln_fb, resilience_tmax_fb, resilience_genus, resilience_family,
-         m, m_fl, m_fb, m_tmax_fb, m_vonb_fb, m_genus, m_family,
-         everything())
+                         ifelse((linf_cm>=40 & !is.na(linf_cm)) | (lmax_cm>=50 & !is.na(lmax_cm)), "large pelagic", "small pelagic"), lh_catg2)) %>% 
+  # Fill resilience gaps
+  mutate(resilience=ifelse(!is.na(resilience_fb), resilience_fb,
+                           ifelse(vulnerability<30, "High",
+                                  ifelse(vulnerability>=30 & vulnerability<55, "Medium",
+                                         ifelse(vulnerability>=55 & vulnerability<70, "Low", 
+                                                ifelse(vulnerability>=70, "Very low", NA))))),
+         resilience=ifelse(is.na(vonb_k), resilience,
+                           ifelse(vonb_k>=0.4, "High",
+                                  ifelse(vonb_k>=0.2 & vonb_k<0.4, "Medium",
+                                         ifelse(vonb_k>=0.1 & vonb_k<0.2, "Low",
+                                                ifelse(vonb_k<0.1, "Very low", resilience))))),
+         resilience=ifelse(!is.na(resilience), resilience, resil_gen), 
+         resilience=factor(resilience, levels=c("Very low", "Low", "Medium", "High")),
+         resilience_fb=factor(resilience_fb, levels=c("Very low", "Low", "Medium", "High"))) %>% 
+  # Rearrange column order
+  select(sci_name_orig:genus, habitat, lh_catg, lh_catg1, lh_catg2, linf_cm, lmax_cm,
+         resilience_gen, resilience_fb, resilience, 
+         vulnerability, tmax_yr, m, everything())
   
-# Inspect completeness
-complete(lhdata)
+# Inspect species missing life history categories
+table(spp_lh_data$habitat)
+table(spp_lh_data$lh_catg)
+missing_lh <- subset(spp_lh_data, is.na(lh_catg)) # once 3, now 0 (a.k.a, problem fixed)
+
+# Make sure pelagics w/out length stats are actually small pelagics
+# (this is an assumption of the imperfect logical test above)
+# Close, Onykia robusta (Robust clubhook squid) is 2 m long; Calanus finmarchicus is a tiny copepod
+missing_tl <- subset(spp_lh_data, lh_catg1=="pelagic" & is.na(linf_cm) & is.na(lmax_cm))
+spp_lh_data$lh_catg2[spp_lh_data$sci_name_fb=="Onykia robusta"] <- "large pelagic"
+
+# Inspect data
+table(spp_lh_data$lh_catg2)
+apply(spp_lh_data, 2, function(x) sum(is.na(x)))
+
+
+# Small vs. large pelagic plots
+##########################################
+
+# Histogram of pelagic Linf
+ggplot(subset(spp_lh_data, lh_catg1=="pelagic"), aes(x=linf_cm)) +
+  geom_histogram(binwidth=10) + theme_bw() + geom_vline(xintercept=40)
+
+# How does a 40 cm Linf relate to maximum length? 50 cm max length
+lmfit <- lm(lmax_cm ~ linf_cm, spp_lh_data, subset=lh_catg1=="pelagic")
+predict(lmfit, newdata=data.frame(linf_cm=40))
+ggplot(subset(spp_lh_data, lh_catg1=="pelagic"), aes(x=linf_cm, y=lmax_cm)) +
+  geom_point() + theme_bw() + geom_vline(xintercept=40) + geom_hline(yintercept=50)
+
+# Histogram of pelagic maximum length
+ggplot(subset(spp_lh_data, lh_catg1=="pelagic"), aes(x=lmax_cm)) +
+  geom_histogram(binwidth=10) + theme_bw()
+
+
+# Resilience plots
+##########################################
+
+# For species without resilience, do they have more K or vulnerability data?
+sum(!is.na(spp_lh_data$vonb_k[is.na(spp_lh_data$resilience_fb)]))
+sum(!is.na(spp_lh_data$vulnerability[is.na(spp_lh_data$resilience_fb)]))
+
+# How is resilience organized by K and lmax?
+ggplot(spp_lh_data, aes(x=lmax_cm, y=vonb_k, color=resilience)) +
+  geom_point() + theme_bw() + 
+  labs(x="Lmax (cm)", y="Von B K") +
+  scale_color_discrete(name="Resilience")
+
+# Von B K is based on life history and is similar to vulnerability
+ggplot(spp_lh_data, aes(y=vonb_k, x=resilience_fb, color=resilience_fb)) +
+  geom_boxplot() + theme_bw() + scale_y_continuous(limits = c(0, 2)) +
+  geom_hline(yintercept=c(0.40, 0.20, 0.1))
+
+# Vulnerability is similar to Von B K but isn't based on a single trait
+ggplot(spp_lh_data, aes(y=vulnerability, x=resilience_fb, color=resilience_fb)) +
+  geom_boxplot() + theme_bw() + geom_hline(yintercept=c(30, 55, 70))
+
+# Lmax isn't as good as Von B K or vulnerability
+ggplot(spp_lh_data, aes(y=lmax_cm, x=resilience_fb, color=resilience_fb)) +
+  geom_boxplot() + theme_bw() + scale_y_continuous(limits = c(0, 300))
 
 
 # Export data
 ################################################################################
 
+# Export for LH classification
+# I used this to create the key which I now read in at the top
+# key <- select(spp_lh_data, type:genus, sci_name_orig:comm_name, habitat)
+# write.csv(key, paste(datadir, "family_to_mprm_lh_catg_key.csv", sep="/"), row.names=F)
+
 # Export data
-save(lhdata,
+save(spp_lh_data,
      file=paste(datadir, "FAO_marine_species_resilience_plus.Rdata", sep="/"))
 
