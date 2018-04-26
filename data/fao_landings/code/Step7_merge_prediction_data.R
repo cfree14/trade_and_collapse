16aca08564d85e19a7a402a13d29c496068f5700
cfree14 <cfree14@gmail.com>
2018-04-26 15:09:02 -0400

    merged prediction data

--- data/fao_landings/code/Step7_merge_prediction_data.R
+++ data/fao_landings/code/Step7_merge_prediction_data.R
@@ -19,13 +19,19 @@ catch <- data; rm(data)
 
 # Read COM predictions
 mprm <- read.csv(paste(preddir, "1950_2017_FAO_bbmsy_timeseries_mprm.csv", sep="/"), as.is=T)
-# cmsy13 <- read.csv(paste(preddir, "1950_2017_FAO_bbmsy_timeseries_cmsy13.csv", sep="/"), as.is=T)
-# comsir <- read.csv(paste(preddir, "1950_2017_FAO_bbmsy_timeseries_comsir.csv", sep="/"), as.is=T)
-# sscom <- read.csv(paste(preddir, "1950_2017_FAO_bbmsy_timeseries_sscom.csv", sep="/"), as.is=T)
+cmsy13 <- read.csv(paste(preddir, "1950_2017_FAO_bbmsy_timeseries_cmsy13.csv", sep="/"), as.is=T)
+comsir <- read.csv(paste(preddir, "1950_2017_FAO_bbmsy_timeseries_comsir.csv", sep="/"), as.is=T)
+ocom <- read.csv(paste(preddir, "1950_2017_FAO_bbmsy_timeseries_ocom.csv", sep="/"), as.is=T)
 
 # Read spectral density data
 dens <- read.csv(paste(preddir, "1950_2017_FAO_spectral_densities.csv", sep="/"), as.is=T)
 
+# What's wrong with OCOM
+length(1950:2015)
+ocom_info <- ocom %>%
+  group_by(stockid) %>% 
+  summarize(n=n()) %>% 
+  filter(n>66)
 
 # Merge data
 ################################################################################
@@ -34,18 +40,27 @@ dens <- read.csv(paste(preddir, "1950_2017_FAO_spectral_densities.csv", sep="/")
 data <- dens %>%
   # Add mPRM predictions
   left_join(select(mprm, stockid, year, bbmsy_q50), by=c("stockid", "year")) %>% 
-  rename(mprm=bbmsy_q50) 
+  rename(mprm=bbmsy_q50) %>% 
+  # Add Catch-MSY predictions
+  left_join(select(cmsy13, stockid, year, bbmsy_q50), by=c("stockid", "year")) %>%
+  rename(cmsy13=bbmsy_q50) %>%
+  # Add COMSIR predictions
+  left_join(select(comsir, stockid, year, bbmsy_q50), by=c("stockid", "year")) %>%
+  rename(comsir=bbmsy_q50) %>% 
+  # Add OCOM predictions
+  left_join(select(ocom, stockid, year, q0.5), by=c("stockid", "year")) %>%
+  rename(ocom=q0.5)
 
 # Inspect completeness
 apply(data, 2, function(x) sum(is.na(x)))
 
-  # # Add Catch-MSY predictions
-  # left_join(select(cmsy13, stockid, year, bbmsy_q50), by=c("stockid", "year")) %>% 
-  # rename(cmsy13=bbmsy_q50) %>% 
-  # # Add COMSIR predictions
-  # left_join(select(comsir, stockid, year, bbmsy_q50), by=c("stockid", "year")) %>% 
-  # rename(comsir=bbmsy_q50)
-  
+
+# Export data
+################################################################################
+
+# Export data
+write.csv(data, paste(preddir, "1950_2017_FAO_bbmsy_timeseries_merge.csv", sep="/"), row.names=F)
+
   
 
 
